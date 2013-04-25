a20f966cdf93298fe93daa71b64b65f7c1b648e7
PigeonNerd <yingchaoliu2@gmail.com>
2013-04-25 16:44:16 -0400

    prescan works now

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void primitive_select(int N, int inData[], int outData[]);
 void primitive_scan(int N, int inData[], int outData[]);
 
 int main(int argc, char** argv) {
-	 int NUM_TUPPLES  = 512;
+	int NUM_TUPPLES  = 512;
 	int* relation = new int[NUM_TUPPLES];
 	 int* result = new int[NUM_TUPPLES];
 	for(int i = 0; i < NUM_TUPPLES; i++) {
@@ -25,6 +25,6 @@ int main(int argc, char** argv) {
 			printf("%d, ", result[i]);
 	}
 	printf("\n");
-
+    //primitive_scan(5, NULL, result);
 	return 0;
 }
--- primitives_test.cu
+++ primitives_test.cu
@@ -3,12 +3,15 @@
 #include <cuda_runtime.h>
 #include <driver_functions.h>
 #include "device_functions.h"
+#include <iostream>
 
 //#include "CycleTimer.h"
 #define SCAN_BLOCK_DIM 256
 #define uint unsigned int
 #include "exclusiveScan.cu_inl"
 #include "cuPrintf.cu"
+
+using namespace std;
 
 __global__ void prescan(int *g_odata, int *g_idata, int n){
 
@@ -17,8 +20,6 @@ __global__ void prescan(int *g_odata, int *g_idata, int n){
     int offset = 1;
     temp[2*thid] = g_idata[2*thid]; // load input into shared memory
     temp[2*thid+1] = g_idata[2*thid+1];
-
-
     for (int d = n>>1; d > 0; d >>= 1){ // build sum in place up the tree
         __syncthreads();
         if (thid < d){
@@ -29,14 +30,11 @@ __global__ void prescan(int *g_odata, int *g_idata, int n){
         }
         offset *= 2;
     }
-
     if (thid == 0) { temp[n - 1] = 0; } // clear the last element
-
     for (int d = 1; d < n; d *= 2){ // traverse down tree & build scan
         offset >>= 1;
         __syncthreads();
         if (thid < d){
-
             int ai = offset*(2*thid+1)-1;
             int bi = offset*(2*thid+2)-1;
             int t = temp[ai];
@@ -44,9 +42,7 @@ __global__ void prescan(int *g_odata, int *g_idata, int n){
             temp[bi] += t;
         }
     }
-
     __syncthreads();
-
     g_odata[2*thid] = temp[2*thid]; // write results to device memory
     g_odata[2*thid+1] = temp[2*thid+1];
 }
@@ -97,7 +93,6 @@ __global__ void tmpScan(int numBlocks, int* result_size, int* histogram) {
 __global__ void coalesced(int N, int* result, int* result_size, int* histogram, int* out) {
 	int threadIndex =  threadIdx.x;
 	int partition = blockIdx.x *  blockDim.x;
-	//cuPrintf("**%d\n", partition);
     if( threadIndex < result_size[blockIdx.x] ) {
 		out[histogram[blockIdx.x] + threadIndex] = result[partition + threadIndex];
 	}
@@ -108,54 +103,58 @@ void
 primitive_scan(int N, int inData[], int outData[]) {
 	int* device_in;
 	int* device_result;
-	cudaMalloc((void**) &device_in, sizeof(int) * 5);
-	cudaMalloc((void**) &device_result, sizeof(int) * 5);
+	cudaMalloc((void**) &device_in, sizeof(int) * 8);
+	cudaMalloc((void**) &device_result, sizeof(int) * 8);
 	
-	int billy [5] = { 16, 2, 77, 40, 12071 };
-	cudaMemcpy(device_in, billy, sizeof(int) * 5, cudaMemcpyHostToDevice);
-	prescan<<<1, 256, 5 * sizeof(int) >>>(device_result, device_in, 5);
-	cudaMemcpy(outData, device_result, sizeof(int) * 5, cudaMemcpyDeviceToHost);
+	int billy [8] = { 16, 2, 77, 40, 12071 , 222, 333, 444};
+	cudaMemcpy(device_in, billy, sizeof(int) * 8, cudaMemcpyHostToDevice);
+	cudaPrintfInit();
+	prescan<<<2, 2, 16 * sizeof(int) >>>(device_result, device_in, 8);
+	cudaThreadSynchronize();
+	cudaMemcpy(outData, device_result, sizeof(int) * 8, cudaMemcpyDeviceToHost);
+
+    for(int i = 0; i < 8 ; i ++) {
+        cout<< outData[i] << ", ";
+    }
+    cout << endl;
+	cudaPrintfDisplay(stdout, true);
+ 	cudaPrintfEnd();
 }
 
 void 
 primitive_select(int N, int inData[], int outData[]) {
 	const int threadPerBlock = 256;
 	const int blocks = (N + threadPerBlock - 1) / threadPerBlock;
-	printf("Num of tuples %d\n", N);
+	const int blocksOfReulstSize = ( blocks + threadPerBlock - 1) / threadPerBlock;
+    printf("Num of tuples %d\n", N);
 	printf("Num of blocks %d\n", blocks);
-	int* device_in;
+	printf("Num of blocks for result size %d\n", blocksOfReulstSize);
+    int* device_in;
 	int* device_result;
 	int* result_size;
 	int* histogram;
 	int* out;
 
 	int* tmp = (int*)calloc(N, sizeof(int));
-
 	cudaMalloc((void**) &device_in, sizeof(int) * N);
 	cudaMalloc((void**) &device_result, sizeof(int) * N);
 	cudaMalloc((void**) &out, sizeof(int) * N);
-
 	cudaMalloc((void**) &result_size, sizeof(int) * blocks);
 	cudaMalloc((void**) &histogram, sizeof(int) * blocks);
 
-	//double startTime = CycleTimer::currentSeconds();
 	cudaMemcpy(device_in, inData, sizeof(int) * N, cudaMemcpyHostToDevice);
 	cudaMemcpy(device_result, tmp, sizeof(int) * N, cudaMemcpyHostToDevice);
 	cudaMemcpy(out, tmp, sizeof(int) * N, cudaMemcpyHostToDevice);
 	cudaMemcpy(result_size, tmp, sizeof(int) * blocks, cudaMemcpyHostToDevice);
-	//double startTime_inner = CycleTimer::currentSeconds();
-	cudaPrintfInit();
+	
+    cudaPrintfInit();
 	primitive_select_kernel<<<blocks, threadPerBlock>>>(N, device_in, device_result, result_size);
 	cudaThreadSynchronize();
-	tmpScan<<<1, threadPerBlock>>>(blocks, result_size, histogram);
-	int size[2];
-	cudaMemcpy(size, histogram, sizeof(int) * blocks, cudaMemcpyDeviceToHost);
-	printf("**%d %d**\n", size[0], size[1]);
+	prescan<<<blocksOfReulstSize, threadPerBlock, threadPerBlock * 2 * sizeof(int)>>>(histogram, result_size, blocks);
+    
 	coalesced<<<blocks, threadPerBlock>>>(N, device_result, result_size, histogram, out);
 	cudaPrintfDisplay(stdout, true);
  	cudaPrintfEnd();
-	//double endTime_inner = CycleTimer::currentSeconds();
-	//prescan<<<1, 512>>>(device_result2, device_result, N);
 	cudaMemcpy(outData, out, sizeof(int) * N, cudaMemcpyDeviceToHost);
 
     cudaFree(device_in);
@@ -163,11 +162,6 @@ primitive_select(int N, int inData[], int outData[]) {
     cudaFree(out);
     cudaFree(result_size);
     cudaFree(histogram);
-	//double endTime = CycleTimer::currentSeconds();
-
-	//double overallDuration = endTime - startTime;
-    //double kernelDuration = endTime_inner - startTime_inner;
-    //printf("Overall: %.3f ms, GPU: %.3f\n", 1000.f * overallDuration, 1000.f * kernelDuration);
 }	
 
 void
