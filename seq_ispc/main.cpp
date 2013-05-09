58d170683b9ab2028661982fd8bca858baed2013
Yingchao Liu <yingchaoliu2@gmail.com>
2013-05-09 17:52:26 -0400

    add ispc

---
+++ seq_ispc/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <algorithm>
+#include <getopt.h>
+
+#include "CycleTimer.h"
+#include "seq_ispc.h"
+
+
+void sequential_select(int N, int inData[], int outData[]);
+
+using namespace ispc;
+
+int main() {
+
+
+	return 0;
+}
+
+
+
+
+
+void sequential_select_dumb(int N, int inData[], int outData[]) {
+    int counter = 0;
+    for(int i = 0; i < N; i ++) {
+        if(inData[i] % 2 == 0) {
+            outData[counter] = inData[i]; 
+            counter++;
+        }
+    }
+}
---
+++ seq_ispc/seq_ispc.h
@@ -0,0 +1,16 @@
+#ifndef ISPC_SEQ_ISPC_H
+#define ISPC_SEQ_ISPC_H
+#include <stdint.h>
+
+#ifdef __cplusplus
+namespce ispc {
+#endif
+
+#if defined(__cplusplus) && !defined(__ISPC_NO_EXTERN_C)
+ extern "C" {
+#endif	
+ 	void mandelbrot_ispc_withtasks( int32_t N, int32_t input[], int32_t output[]);
+}
+}
+#endif
+#endif
