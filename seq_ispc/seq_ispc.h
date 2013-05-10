422d04aeb43557729325610d7b47df99b8bee8f4
PigeonNerd <yingchaoliu2@gmail.com>
2013-05-10 14:23:04 -0400

    add openmp

--- seq_ispc/seq_ispc.h
+++ seq_ispc/seq_ispc.h
@@ -9,7 +9,7 @@ namespace ispc {
 #if defined(__cplusplus) && !defined(__ISPC_NO_EXTERN_C)
  extern "C" {
 #endif	
- 	void mandelbrot_ispc_withtasks( int32_t N, int32_t input[], int32_t output[]);
+ 	void select_ispc_withtasks( int32_t N, int32_t input[], int32_t output[]);
 #if defined(__cplusplus) && !defined(__ISPC_NO_EXTERN_C)    
 }
 #endif
